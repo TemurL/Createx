3f09e8c0dbed309b5aa88a51bbc2e248f400f1eb
Temur Lakerbaya <lakerbaya00@mail.ru>
2023-10-09 14:06:25 +0300

    update slider

--- src/js/modules/heroSlider.js
+++ src/js/modules/heroSlider.js
@@ -1,25 +1,47 @@
 const leftBtn = document.getElementById('slider-button-to-left');
 const rightBtn = document.getElementById('slider-button-to-right');
 const wrapper = document.getElementsByClassName('slider__wrapper')[0];
+const slider = document.getElementsByClassName('slider')[0];
+
+const setGridTamplate = () => {
+    let blocksQtt = wrapper.children.length;
+    wrapper.style.setProperty("--blocksQtt", blocksQtt);
+    wrapper.setAttribute('show', 1);
+    slider.setAttribute('show', 1);
+}
 
 const moveToRight = () => {
     let show = Number(wrapper.getAttribute('show'));
     if (show < wrapper.children.length) {
-        rightBtn.parentElement.setAttribute('href', `#slider-block-${show + 2}`);
+        rightBtn.parentElement.setAttribute('href', `#slider-block-${show + 1}`);
         leftBtn.parentElement.setAttribute('href', `#slider-block-${show}`);
         wrapper.setAttribute('show', show + 1);
-        console.log(show);
+        slider.classList.add('slider_on-transition');
+        setTimeout(() => {
+            slider.setAttribute('show', show + 1);
+        }, 500);
+        setTimeout(() => {
+            slider.classList.remove('slider_on-transition');
+        }, 1000);
+
     }
 }
 const moveToLeft = () => {
     let show = Number(wrapper.getAttribute('show'));
     if (show > 1) {
         rightBtn.parentElement.setAttribute('href', `#slider-block-${show}`);
-        leftBtn.parentElement.setAttribute('href', `#slider-block-${show - 2}`);
+        leftBtn.parentElement.setAttribute('href', `#slider-block-${show - 1}`);
         wrapper.setAttribute('show', show - 1);
-        console.log(show);
+        slider.classList.add('slider_on-transition');
+        setTimeout(() => {
+            slider.setAttribute('show', show - 1);
+        }, 500);
+        setTimeout(() => {
+            slider.classList.remove('slider_on-transition');
+        }, 1000);
     }
 }
 
-rightBtn.addEventListener('click', () => setTimeout(moveToRight, 60));
-leftBtn.addEventListener('click', () => setTimeout(moveToLeft, 60));
+setGridTamplate();
+rightBtn.addEventListener('click', moveToRight);
+leftBtn.addEventListener('click', moveToLeft);
