b5de082c53164969e462da3138bd30b119a2bb0d
Temur Lakerbaya <lakerbaya00@mail.ru>
2023-10-17 00:30:31 +0300

    one more section and some fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 import * as TLFunc from './modules/functions.js';
 import { heroSlider } from './modules/heroSlider.js';
 import { mobMenu } from './modules/mobMenu.js';
+import { portfolioCard } from './modules/portfolioCard.js';
 import { serviceCardHover } from './modules/serviceCardHover.js';
 
 TLFunc.isWebp()
 
 heroSlider();
 mobMenu();
-serviceCardHover()
+serviceCardHover();
+portfolioCard()
