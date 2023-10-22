06cf6398bda452263aff6036a72bab048f7d896c
Temur Lakerbaya <lakerbaya00@mail.ru>
2023-10-22 11:18:36 +0300

    lots of things

--- src/js/modules/headerBack.js
+++ src/js/modules/headerBack.js
@@ -2,7 +2,6 @@ export const headerBack = () => {
     if (document.title !== 'Createx') {
         document.getElementsByClassName('header')[0].classList.add('header_transparent');
         document.addEventListener('scroll', () => {
-            console.log(scrollY);
             if (scrollY !== 0) {
                 document.getElementsByClassName('header')[0].classList.remove('header_transparent');
             } else {
@@ -11,5 +10,14 @@ export const headerBack = () => {
                 }, 400)
             }
           })
-    };
+    }
+
+    let title = document.title;
+    const menuliArr = Array.from(document.getElementsByClassName('nav__li'));
+
+    menuliArr.forEach((li) => {
+        if (li.children[0].textContent === title) {
+            li.classList.add('nav__li_active');
+        }
+    })
 }
