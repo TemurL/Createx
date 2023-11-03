3a5203bea30e8e124a99d137746d4befa3df6718
Temur Lakerbaya <lakerbaya00@mail.ru>
2023-11-03 18:40:15 +0300

    2days pause ended

--- src/js/modules/katalogCollaps.js
+++ src/js/modules/katalogCollaps.js
@@ -2,8 +2,12 @@ export const katalogCollaps = () => {
     if (document.title !== 'Work') return
     const btn = document.getElementsByClassName('katalog__collaps-button')[0];
     const container = document.getElementsByClassName('katalog__content')[0];
-    
+    const katalodBtnArr = Array.from(document.getElementsByClassName('katalog__button'));
     btn.addEventListener('click', () => {
         container.classList.toggle('katalog__content_open');
+        btn.classList.toggle('katalog__collaps-button_open');
+    })
+    katalodBtnArr.forEach(item => {
+        item.addEventListener('click', () => btn.click())
     })
 } 
