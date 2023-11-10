c6b398c1cf88f7754a776d51527f2e8146bd0289
Temur Lakerbaya <lakerbaya00@mail.ru>
2023-11-10 20:45:23 +0300

    maybe final changes

--- src/js/modules/teamSocials.js
+++ src/js/modules/teamSocials.js
@@ -6,6 +6,7 @@ export const teamSocials = () => {
     for (let i = 0; i < teamMembers.length; i++) {
         teamMembers[i].addEventListener('click', () => {
             if (window.innerWidth > 766) return
+            if (!socials[i].classList.contains(socialsModif)) Array.from(socials).forEach(el => el.classList.remove(socialsModif));
             socials[i].classList.toggle(socialsModif);
         })
     }
